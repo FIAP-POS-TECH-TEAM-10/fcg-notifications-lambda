801143940fd2c1e75a5d40e820bcd26fc1cf2e47
Eric Lopes Pedroso <ericlopespedroso@gmail.com>
2026-09-13 13:08:35 -0300

    Ajustes de infra

--- infra/provider.tf
+++ infra/provider.tf
@@ -6,4 +6,17 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  # Backend remoto centralizado (S3 + DynamoDB)
+  backend "s3" {
+    bucket         = "fiap-tech-challenge-tfstate-123456-915153720516-sa-east-1-an" # Substitua pelo seu Bucket S3
+    key            = "notification-lambda/terraform.tfstate" # Key exclusiva deste microsserviço
+    region         = "sa-east-1"                          # Sua região AWS
+    dynamodb_table = "fiap-tech-challenge-tflocks"        # Nome da sua tabela DynamoDB
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
 }
